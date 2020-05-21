c5f3dc04bf574b29918a65ff99c21602fe270b09
Evgen <Evgennsk54@gmail.com>
2020-05-21 10:51:54 +0700

    Initial Commit

---
+++ ContactListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactListApp
+//
+//  Created by Евгений Каратаев on 21.05.2020.
+//  Copyright © 2020 Evgen Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

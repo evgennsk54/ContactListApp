e04381f6b4f906dbedba5a6aa66f3a8e5a13991d
Evgen <Evgennsk54@gmail.com>
2020-05-22 12:42:35 +0700

    Row Height

--- ContactListApp/ContactListViewController.swift
+++ ContactListApp/ContactListViewController.swift
@@ -36,10 +36,17 @@ class ContactListViewController: UITableViewController {
         
         cell.textLabel?.text = contactList[indexPath.row]
         cell.imageView?.image = UIImage(named: contactList[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         
         return cell
     }
 
+    // MARK: - Table View Delegate
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
     /*
     // MARK: - Navigation
 
